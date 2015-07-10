af08b796703931e90df44c909605f615037f6aef
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2015-07-10 16:37:55 +0200

    ajout routes (status et status-rest), factories controller (Status et StatusRest) et factories service (Module et Status)

--- module/Ent/config/module.config.php
+++ module/Ent/config/module.config.php
@@ -10,6 +10,8 @@ return array(
             'Ent\Controller\UserRest'    => 'Ent\Factory\Controller\UserRestControllerFactory',
             'Ent\Controller\Module' => 'Ent\Factory\Controller\ModuleControllerFactory',
             'Ent\Controller\ModuleRest' => 'Ent\Factory\Controller\ModuleRestControllerFactory',
+            'Ent\Controller\Status' => 'Ent\Factory\Controller\StatusControllerFactory',
+            'Ent\Controller\StatusRest' => 'Ent\Factory\Controller\StatusRestControllerFactory',
 //            'Ent\Controller\Module' => function($cm) {
 //                $sm = $cm->getServiceLocator($cm);
 //                
@@ -213,6 +215,64 @@ return array(
                     ),
                 ),
             ),
+            'status' => array(
+                'type' => \Zend\Mvc\Router\Http\Literal::class,
+                'options' => array(
+                    'route' => '/status',
+                    'defaults' => array(
+                        'controller' => 'Ent\Controller\Status',
+                        'action' => 'list',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'add' => array(
+                        'type' => \Zend\Mvc\Router\Http\Literal::class,
+                        'options' => array(
+                            'route' => '/add',
+                            'defaults' => array(
+                                'action' => 'add',
+                            ),
+                        ),
+                    ),
+                    'show' => array(
+                        'type' => Zend\Mvc\Router\Http\Segment::class,
+                        'options' => array(
+                            'route' => '/show/:id',
+                            'defaults' => array(
+                                'action' => 'show',
+                            ),
+                            'constraints' => array(
+                                'id' => '[1-9][0-9]*'
+                            ),
+                        ),
+                    ),
+                    'update' => array(
+                        'type' => Zend\Mvc\Router\Http\Segment::class,
+                        'options' => array(
+                            'route' => '/update/:id',
+                            'defaults' => array(
+                                'action' => 'update',
+                            ),
+                            'constraints' => array(
+                                'id' => '[1-9][0-9]*'
+                            ),
+                        ),
+                    ),
+                    'delete' => array(
+                        'type' => Zend\Mvc\Router\Http\Segment::class,
+                        'options' => array(
+                            'route' => '/delete/:id',
+                            'defaults' => array(
+                                'action' => 'delete',
+                            ),
+                            'constraints' => array(
+                                'id' => '[1-9][0-9]*'
+                            ),
+                        ),
+                    ),
+                ),
+            ),
             'user-rest' => array(
                 'type'    => \Zend\Mvc\Router\Http\Segment::class,
                 'options' => array(
@@ -234,6 +294,18 @@ return array(
                     ),
                     'defaults' => array(
                         'controller' => 'Ent\Controller\ModuleRest',
+                    ),
+                ),
+            ),
+            'status-rest' => array(
+                'type'    => \Zend\Mvc\Router\Http\Segment::class,
+                'options' => array(
+                    'route'    => '/status-rest[/:id]',
+                    'constraints' => array(
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Ent\Controller\StatusRest',
                     ),
                 ),
             ),
@@ -264,13 +336,15 @@ return array(
         'factories' => array( 
 //            'AddressBook\Service\ContactZendDb' => 'AddressBook\Factory\Service\ContactZendDbServiceFactory',
             'Ent\Service\UserDoctrineORM' => 'Ent\Factory\Service\UserDoctrineORMServiceFactory',
-            'Ent\Service\Module' => function($sm) {
-
-                $om = $sm->get('Doctrine\ORM\EntityManager');
-                $service = new \Ent\Service\ModuleDoctrineService($om);
-
-                return $service;
-            },
+            'Ent\Service\Module' => 'Ent\Factory\Service\ModuleDoctrineORMServiceFactory',
+            'Ent\Service\Status' => 'Ent\Factory\Service\StatusDoctrineORMServiceFactory',
+//            'Ent\Service\Module' => function($sm) {
+//
+//                $om = $sm->get('Doctrine\ORM\EntityManager');
+//                $service = new \Ent\Service\ModuleDoctrineService($om);
+//
+//                return $service;
+//            },
         ),
         'aliases' => array(
 //            'AddressBook\Service\Contact' => 'AddressBook\Service\ContactFake'
