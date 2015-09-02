ec9dae5e1c01539468b0a08ca3ec89f5da9ca9f1
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2015-09-02 11:20:25 +0200

    test unitaire Entity Preference

---
+++ module/Ent/test/EntTest/Entity/EntPreferenceTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EntTest\Entity;
+
+use Ent\Entity\EntPreference;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Description of EntPreferenceTest
+ *
+ * @author mdjimbi
+ */
+class EntPreferenceTest extends \PHPUnit_Framework_TestCase
+{
+    protected $preference;
+    
+    protected function setUp() {
+        $this->preference = new EntPreference();
+    }
+    
+    public static function setUpBeforeClass() {
+        require_once __DIR__. '/../../../src/Ent/Entity/EntPreference.php';
+    }
+    
+    protected function tearDown() {
+        parent::tearDown();
+    }
+    
+    public static function tearDownAfterClass() {
+        parent::tearDownAfterClass();
+    }
+    
+    public function testInitValuesAreNull() {
+        $this->assertNull($this->preference->getPrefId());
+        $this->assertNull($this->preference->getPrefAttribute());
+        $this->assertEmpty($this->preference->getFkPrefProfile());
+        $this->assertEmpty($this->preference->getFkPrefService());
+        $this->assertEmpty($this->preference->getFkPrefStatus());
+        $this->assertEmpty($this->preference->getFkPrefUser());
+    }
+}
