91a19b4483c09615e034d71f25f459f08eec8888
fandria <francis.andria@parisdescartes.fr>
2015-09-11 12:02:41 +0200

    indexRestController : getList is_logged

---
+++ module/Ent/src/Ent/Controller/indexRestController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Ent\Controller;
+
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+/**
+ * Description of indexRestController
+ *
+ * @author fandria
+ */
+class indexRestController extends AbstractRestfulController {
+
+    public function getList()
+    {
+        $is_logged = false;
+        
+        $authService = $this->serviceLocator->get('Zend\Authentication\AuthenticationService');
+        if ($authService->hasIdentity()) {
+            $is_logged = true;
+            var_dump($authService->getIdentity()->getUserLogin());
+        } else {
+            $is_logged = false;
+        }
+        
+        return new JsonModel(array(
+            'is_logged' => $is_logged
+        ));
+    }
+}
