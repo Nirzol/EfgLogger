258de83cf12c19be4fb2b7a22ba009b232fa3854
fandria <francis.andria@parisdescartes.fr>
2015-09-21 17:08:28 +0200

    ServiceAttributeInputFilter

---
+++ module/Ent/src/Ent/InputFilter/ServiceAttributeInputFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ent\InputFilter;
+
+use Zend\InputFilter\InputFilter;
+
+/**
+ * Description of ServiceAttributeInputFilter
+ *
+ * @author fandria
+ */
+class ServiceAttributeInputFilter extends InputFilter {
+    public function __construct()
+    {
+        $this->add(array(
+            'name' => 'serviceAttributeValue',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'NotEmpty',
+                ),
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'fkSaService',
+            'required' => true,
+            'filters' => array(
+            ),
+            'validators' => array(
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'fkSaAttribute',
+            'required' => true,
+            'filters' => array(
+            ),
+            'validators' => array(
+            ),
+        ));
+    }
+}
