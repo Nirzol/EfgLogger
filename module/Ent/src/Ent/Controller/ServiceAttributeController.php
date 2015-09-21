e445e297a5328a928b6d9addcb4dc9359f5d5253
fandria <francis.andria@parisdescartes.fr>
2015-09-21 17:10:24 +0200

    ServiceAttribute controller

---
+++ module/Ent/src/Ent/Controller/ServiceAttributeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ent\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ServiceAttributeController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+    public function listAction()
+    {
+        return new ViewModel();
+    }
+
+    public function showAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
