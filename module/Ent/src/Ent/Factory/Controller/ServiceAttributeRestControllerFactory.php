0c92bdf57122e03699d5dd096923f37f514ec9c8
fandria <francis.andria@parisdescartes.fr>
2015-09-21 17:09:28 +0200

    ServiceAttributeRestControllerFactory

---
+++ module/Ent/src/Ent/Factory/Controller/ServiceAttributeRestControllerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ent\Factory\Controller;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject;
+use Ent\Controller\ServiceRestController;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+/**
+ * Description of ServiceAttributeRestControllerFactory
+ *
+ * @author fandria
+ */
+class ServiceAttributeRestControllerFactory implements FactoryInterface{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        /* @var $serviceLocator ControllerManager */
+        $sm   = $serviceLocator->getServiceLocator();
+        $serviceAttributeService = $sm->get('Ent\Service\ServiceAttributeDoctrineORM');
+
+        $serviceAttributeForm    = $sm->get('FormElementManager')->get('Ent\Form\ServiceAttributeForm');
+
+        /* @var $serviceLocator ObjectManager */
+        $om   = $sm->get('Doctrine\ORM\EntityManager');
+        $hydrator = new DoctrineObject($om);
+
+        $controller = new ServiceAttributeRestController($serviceAttributeService, $serviceAttributeForm, $hydrator);
+
+        return $controller;
+    }
+}
