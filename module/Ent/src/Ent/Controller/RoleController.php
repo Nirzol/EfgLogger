f41ad83e48b5c1940825088747173d6827024d61
Nirzol <grondin.eric@gmail.com>
2015-09-23 14:11:32 +0200

    addaction role

--- module/Ent/src/Ent/Controller/RoleController.php
+++ module/Ent/src/Ent/Controller/RoleController.php
@@ -7,17 +7,18 @@ use Zend\View\Model\ViewModel;
 
 class RoleController extends AbstractActionController
 {
+
     /**
      * @var RoleForm
      */
     protected $roleForm = null;
-    
+
     public function __construct(\Ent\Service\RoleDoctrineService $roleService, \Ent\Form\RoleForm $roleForm)
     {
         $this->roleService = $roleService;
         $this->roleForm = $roleForm;
     }
-    
+
     public function listAction()
     {
         $roles = $this->roleService->getAll();
@@ -25,6 +26,27 @@ class RoleController extends AbstractActionController
         return new ViewModel(array(
             'roles' => $roles,
         ));
+    }
+
+    public function addAction()
+    {
+//        $form = $this->roleForm;
+//
+//        if ($this->request->isPost()) {
+//            $data = $this->request->getPost();
+//            $role = $this->roleService->insert($form, $data);
+//
+//            if ($role) {
+//                $this->flashMessenger()->addSuccessMessage('Le role a bien été insérer.');
+//
+//                return $this->redirect()->toRoute('role');
+//            }
+//        }
+//
+//        return new ViewModel(array(
+//            'form' => $form->prepare(),
+//        ));
+        return new ViewModel();
     }
 
 
