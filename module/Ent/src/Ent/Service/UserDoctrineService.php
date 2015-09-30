5977047b9c6636b4200f1d149c61a16a95df3277
sebbar <thierry.sebbar@parisdescartes.fr>
2015-09-30 17:32:42 +0200

    Fixing

--- module/Ent/src/Ent/Service/UserDoctrineService.php
+++ module/Ent/src/Ent/Service/UserDoctrineService.php
@@ -92,23 +92,6 @@ class UserDoctrineService implements UserServiceInterface
         return $repoFindOneBy;
     }
 
-    public function getByLogin($login)
-    {
-        $eoUser = NULL;
-
-        try {
-            $repo = $this->em->getRepository('Ent\Entity\EntUser');
-            $users = $repo->findBy(array('userLogin' => $login));
-            $eoUser = $users[0];
-        } catch (Exception $exc) {
-            $eoUser = NULL;
-            error_log($exc->getTraceAsString());
-            return NULL;
-        }
-
-        return $eoUser;
-    }
-
     public function insert(Form $form, $dataAssoc)
     {
         $user = $this->user;
