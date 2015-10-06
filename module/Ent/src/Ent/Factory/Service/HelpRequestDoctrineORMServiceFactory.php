dbd1e0c4dbc142f0c3c3f362403778344e75a452
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2015-10-06 12:19:32 +0200

    HelpRequest ServiceFactory

---
+++ module/Ent/src/Ent/Factory/Service/HelpRequestDoctrineORMServiceFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ent\Factory\Service;
+
+use Ent\Service\HelpRequestDoctrineService;
+use Doctrine\Common\Persistence\ObjectManager;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Description of HelpRequestDoctrineORMServiceFactory
+ *
+ * @author mdjimbi
+ */
+
+class HelpRequestDoctrineORMServiceFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        /* @var $serviceLocator ObjectManager */
+        $om = $serviceLocator->get('Doctrine\ORM\EntityManager');
+        
+        $service = new HelpRequestDoctrineService($om);
+        
+        return $service;
+    }
+    
+}
