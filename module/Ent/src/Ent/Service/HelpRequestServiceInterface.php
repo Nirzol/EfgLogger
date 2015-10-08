df2178241356f78604ea77f8470abd82424db753
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2015-10-08 11:50:11 +0200

    ajout fonctions sendWithImage et sendWithoutImage

--- module/Ent/src/Ent/Service/HelpRequestServiceInterface.php
+++ module/Ent/src/Ent/Service/HelpRequestServiceInterface.php
@@ -6,5 +6,7 @@ use Zend\Form\Form;
 
 interface HelpRequestServiceInterface 
 {
-    public function send($message, $filePath, $fileName, $senderMail, $senderName, $recipientMail, $recipientName, $subject);
+    public function sendWithImage($message, $filePath, $fileName, $senderMail, $senderName, $recipientMail, $recipientName, $subject);
+    
+    public function sendWithoutImage($subject, $message, $senderMail, $senderName, $recipientMail, $recipientName);
 }
