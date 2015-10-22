df02c18189370c907f46356c3a4c17e72e6e4c19
Nirzol <grondin.eric@gmail.com>
2015-10-22 16:23:52 +0200

    Envoie fichier manquant

--- config/autoload/efgcasauth.global.php
+++ config/autoload/efgcasauth.global.php
@@ -1,17 +1,27 @@
 <?php
 
 // Modify with your own configuration
-return array(
+return array(  
     'cas' => array(
-        'cas_version' => '2.0',
-        'server_hostname' => 'servauth.univ-paris5.fr',
-        'server_port' => 443,
-        'server_path' => '/cas',
+        'cas_version' => '2.0',//'CAS_VERSION_2_0',
+	'cas_hostname' => 'servauth.univ-paris5.fr',
+	'cas_port' => 443,
+	'cas_path' => '/cas',
         'no_account_route' => 'user/add-auto',
         'cas_server_ca_cert_path' => dirname(dirname(__DIR__)) . '/data/certificat_P5_cas.pem',
+        'cas_proxy' => true,
+        'cas_imap_name' => 'imap-i.infr.univ-paris5.fr', //'imap.univ-paris5.fr:993',
+        'cas_imap_password' => '',
+        'cas_pgt_dir' => dirname(dirname(__DIR__)) . '/data/',
+        'cas_debug_file' => dirname(dirname(__DIR__)) . '/data/cas_debug.log',
+        'cas_debug' => true,
+        'cas_validation' => 'ca',
+        'cas_login_url' => '',
+        'cas_logout_url' => '',
+        'cas_redirect_route_after_login' => 'home',
     ),
     'get_form' => array(
-        'user_form' => 'Ent\Form\UserForm',
+        'user_form' => 'Ent\Form\UserForm',        
     ),
     'doctrine' => array(
         'user_doctrine_orm_service_factory' => 'Ent\Service\UserDoctrineORM',
@@ -31,5 +41,5 @@ return array(
 //                },
             ),
         ),
-    ),
+    ), 
 );
