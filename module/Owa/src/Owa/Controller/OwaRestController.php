5d4038ecaea7dbf588c72fc1fd3c3f87ea466dbb
fandria <francis.andria@parisdescartes.fr>
2015-11-09 17:54:51 +0100

    owa rest controller begin

---
+++ module/Owa/src/Owa/Controller/OwaRestController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Owa\Controller;
+
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+/**
+ * Description of OwaRestController
+ *
+ * @author fandria
+ */
+class OwaRestController extends AbstractRestfulController {
+    public function __construct()
+    {
+        
+    }
+
+    public function getList()
+    {
+        $data = 'owa controller';
+        $success = true;
+        $successMessage = 'ok';
+        $errorMessage = '';
+        
+        return new JsonModel(array(
+            'data' => $data,
+            'success' => $success,
+            'flashMessages' => array(
+                'success' => $successMessage,
+                'error' => $errorMessage,
+            ),
+        ));
+    }
+    
+    public function getMailNotifAction() {
+        $data = 200;
+        $success = true;
+        $successMessage = 'ok';
+        $errorMessage = '';
+        
+        return new JsonModel(array(
+            'data' => $data,
+            'success' => $success,
+            'flashMessages' => array(
+                'success' => $successMessage,
+                'error' => $errorMessage,
+            ),
+        ));
+    }
+    
+    public function getCalendarNotifAction() {
+        $data = 10;
+        $success = true;
+        $successMessage = 'ok';
+        $errorMessage = '';
+        
+        return new JsonModel(array(
+            'data' => $data,
+            'success' => $success,
+            'flashMessages' => array(
+                'success' => $successMessage,
+                'error' => $errorMessage,
+            ),
+        ));
+    }
+}
