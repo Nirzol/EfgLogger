24c2e2baafe4bf866f5489259fd281bbdd3088f7
fandria <francis.andria@parisdescartes.fr>
2015-11-12 11:19:23 +0100

    owa model controller : does not work

---
+++ module/Owa/src/Owa/Factory/Model/OwaFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Owa\Factory\Model;
+
+use Owa\Model\Owa;
+use PhpEws\EwsConnection;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Description of OwaFactory
+ *
+ * @author fandria
+ */
+class OwaFactory implements FactoryInterface {
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        /* @var $serviceLocator ControllerManager */
+        $sm = $serviceLocator->getServiceLocator();
+
+        $config = $sm->get('config');
+        
+        $ews = new EwsConnection($config['owa_config']['host'], $config['owa_config']['username'], $config['owa_config']['password'], $config['owa_config']['version']);
+        
+        $owa = new Owa($ews);
+        
+        return $owa;
+    }
+}
