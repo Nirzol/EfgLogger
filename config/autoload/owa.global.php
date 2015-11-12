e612178a9ed2d0f3b825cdbd84efe918e0f45e54
fandria <francis.andria@parisdescartes.fr>
2015-11-12 11:17:16 +0100

    config module owa

---
+++ config/autoload/owa.global.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'owa_config' => array(
+        'host'                  => 'owa.parisdescartes.fr',
+        'version'               => 'Exchange2013',
+        'username'              => 'svc-ent',
+        'password'              => 'Olive4Ever!',
+    ),
+    
+);
