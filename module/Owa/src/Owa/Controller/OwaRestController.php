a4942952a387e1e4fe34ce9a2bbfec900828aa11
fandria <francis.andria@parisdescartes.fr>
2015-11-13 14:24:19 +0100

    owa controller : comment lines

--- module/Owa/src/Owa/Controller/OwaRestController.php
+++ module/Owa/src/Owa/Controller/OwaRestController.php
@@ -92,19 +92,53 @@ class OwaRestController extends AbstractRestfulController {
         ));
     }
     
-    public function getCalendarNotifAction() {
-        $data = 10;
-        $success = true;
-        $successMessage = 'ok';
-        $errorMessage = '';
-        
-        return new JsonModel(array(
-            'number' => $data,
-            'success' => $success,
-            'flashMessages' => array(
-                'success' => $successMessage,
-                'error' => $errorMessage,
-            ),
-        ));
-    }
+//    public function getCalendarNotifAction() {
+//        $login = 'fandria';
+//        
+////        $login = null;
+////        $authService = $this->serviceLocator->get('Zend\Authentication\AuthenticationService');
+////        if ($authService->hasIdentity()) {
+////            $login = $authService->getIdentity()->getUserLogin();
+////        }
+//
+//        $data = null;
+//        $success = false;
+//        $successMessage = '';
+//        $errorMessage = '';
+//        
+//        $mail = null;
+//        if (!is_null($login)) {
+//            $mail = $this->searchLdapController->getMailByUid($login);
+//            $owa = $this->owa;
+//            if (!is_null($owa)) {
+//                $owa->setImpersonation($mail);
+//                $calendar = $owa->getCalendarEvents();
+//                
+//                $data = $calendar;
+//                
+////                if (!is_null($mails)) {
+////                    $number = $owa->getNumberOfUnread($mails);
+////                
+////                    $data = $number;
+////                    $success = true;
+////                    $successMessage = 'ok';
+////                    $errorMessage = '';
+////                } else {
+////                    $errorMessage = 'Calendriers non récupérés';
+////                }
+//               
+//            }
+//        } else {
+//            $errorMessage = 'User non authentifié';
+//        }
+//        
+//        return new JsonModel(array(
+//            'number' => $data,
+//            'success' => $success,
+//            'flashMessages' => array(
+//                'success' => $successMessage,
+//                'error' => $errorMessage,
+//            ),
+//        ));
+//    }
 }
