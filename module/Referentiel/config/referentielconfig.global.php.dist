c7fcdd6a1bc6419e157cc5aa73d41799f8189428
fandria <francis.andria@parisdescartes.fr>
2015-12-16 16:02:31 +0100

    referentiel : crypt is there

---
+++ module/Referentiel/config/referentielconfig.global.php.dist
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'referentiel_config' => array(
+        'wsdl' => 'http://app.parisdescartes.fr/cgi-bin/WebObjects/ReferentielWS.woa/ws/Referentiel?wsdl'
+    ),
+    
+);
---
+++ module/Referentiel/src/Referentiel/Factory/Service/ReferentielServiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Referentiel\Factory\Service;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Description of ReferentielServiceFactory
+ *
+ * @author fandria
+ */
+class ReferentielServiceFactory implements FactoryInterface{
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        /* @var $serviceLocator ControllerManager */
+        $sm = $serviceLocator->getServiceLocator();
+
+        $config = $sm->get('Config');
+        
+        $client = new Zend\Soap\Client($config['referentiel_config']['wsdl']);
+
+        $referentiel = new \Referentiel($client);
+
+        return $referentiel;
+    }
+}
