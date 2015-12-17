b9afc0955ebd66286609dea8bc0f852e9210aa64
fandria <francis.andria@parisdescartes.fr>
2015-12-17 18:15:25 +0100

    delete owa log from files

--- config/autoload/owa.global.php
+++ config/autoload/owa.global.php
@@ -4,8 +4,6 @@ return array(
     'owa_config' => array(
         'host'                  => 'owa.parisdescartes.fr',
         'version'               => 'Exchange2013',
-        'username'              => '0ZvykmFuqh0RAsa3GXB4zP',
-        'password'              => 'kKefwdA0YRtX72lhJeowo',
     ),
     
 );
