d202e327d28957139b2e9fe33777ca2a6d4d809b
Nirzol <grondin.eric@gmail.com>
2015-12-17 10:04:10 +0100

    Mise en place du cache pour le serveur de PROD.

--- config/application.config.php
+++ config/application.config.php
@@ -47,26 +47,31 @@ return array(
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
         //'config_cache_enabled' => $booleanValue,
-//        'config_cache_enabled' => ($env == 'production'),
+        'config_cache_enabled' => ($env == 'production'),
+        
         // The key used to create the configuration cache file name.
         //'config_cache_key' => $stringKey,
-//        'config_cache_key' => 'app_config',
+        'config_cache_key' => 'app_config',
+        
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
         //'module_map_cache_enabled' => $booleanValue,
-//        'module_map_cache_enabled' => ($env == 'production'),
+        'module_map_cache_enabled' => ($env == 'production'),
+        
         // The key used to create the class map cache file name.
         //'module_map_cache_key' => $stringKey,
-//        'module_map_cache_key' => 'module_map',
+        'module_map_cache_key' => 'module_map',
+        
         // The path in which to cache merged configuration.
         //'cache_dir' => $stringPath,
-//        'cache_dir' => 'data/cache/modulecache',
+        'cache_dir' => 'data/cache/modulecache',
+        
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
         // that weren't loaded.
         // 'check_dependencies' => true,
-//        'check_dependencies' => ($env != 'production'),
+        'check_dependencies' => ($env != 'production'),
     ),
         // Used to create an own service manager. May contain one or more child arrays.
         //'service_listener_options' => array(
