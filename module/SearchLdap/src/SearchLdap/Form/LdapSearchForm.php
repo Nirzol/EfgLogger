638cc53ea77c30bc6f5d69fcd6904ba2d5b0375d
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2016-02-09 17:11:41 +0100

    formulaire pour LdapSearch

---
+++ module/SearchLdap/src/SearchLdap/Form/LdapSearchForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SearchLdap\Form;
+
+use Zend\Form\Form;
+
+/**
+ * Description of LdapSearchForm
+ *
+ * @author mdjimbi
+ */
+
+class LdapSearchForm extends Form {
+    public function __construct($name = null) {
+        parent::__construct('searchLdap');
+        
+        $this->add(array(
+            'name' => 'searchValue',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Nom, login ou mail : ',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Chercher',
+                'id' => 'submitbutton',
+                'class' => 'btn btn-primary'
+            ),
+        ));
+    }
+}
