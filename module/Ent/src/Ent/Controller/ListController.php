44e05e42e78fdb08246255cec11e360e97a2c46b
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2016-02-09 10:47:43 +0100

    controller list

---
+++ module/Ent/src/Ent/Controller/ListController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ent\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ListController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+        
+    }
+
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+
+    public function listAction()
+    {
+        return new ViewModel();
+    }
+
+    public function updateAction()
+    {
+        return new ViewModel();
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+    public function showAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
