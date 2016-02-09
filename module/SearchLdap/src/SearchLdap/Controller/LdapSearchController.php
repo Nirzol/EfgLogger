299212253268dfdd354f2e3720db2e71e629022e
mdjimbi <mdjimbi@sedre4.dsi.univ-paris5.fr>
2016-02-09 17:10:57 +0100

    controller LdapSearch

---
+++ module/SearchLdap/src/SearchLdap/Controller/LdapSearchController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace SearchLdap\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use SearchLdap\Model\SearchLdap;
+use Zend\Http\Request;
+use SearchLdap\Form\LdapSearchForm;
+use SearchLdap\InputFilter\LdapSearchFilter;
+
+/**
+ * Description of LdapSearchController
+ *
+ * @author mdjimbi
+ */
+
+class LdapSearchController extends AbstractActionController {
+
+    /**
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     *
+     * @var SearchLdap
+     */
+    protected $searchLdapModel;
+    
+    /**
+     *
+     * @var LdapSearchForm
+     */
+    protected $searchLdapForm;
+    
+    /**
+     *
+     * @var LdapSearchFiler
+     */
+    protected $searchLdapFilter;
+
+    public function __construct(SearchLdap $searchLdapModel, LdapSearchForm $searchLdapForm, LdapSearchFilter $searchLdapFilter) {
+        $this->searchLdapModel = $searchLdapModel;
+        $this->searchLdapForm = $searchLdapForm;
+        $this->searchLdapFilter = $searchLdapFilter;
+    }
+
+    public function searchAction() {
+        $form = $this->searchLdapForm;
+
+        $request = $this->request;
+
+        if ($request->isPost()) {
+            $filter = $this->searchLdapFilter;
+
+            $form->setInputFilter($filter);
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) {
+                $search = $this->request->getPost('searchValue');
+                
+                if (!empty($search)) {
+                    $result = $this->searchLdapModel->searchUser($search);
+
+                    if (empty($result)) {
+                        $message = 'Aucune personne trouvée';
+                    }
+                }
+            }
+        }
+
+        return new ViewModel(array(
+            'form' => $form->prepare(),
+            'result' => $result,
+            'message' => $message,
+        ));
+    }
+
+}
